18b4f03b86b4ab2e8abb1e1fd7d46a86a9f347be
Vanshaj Girotra <vanshaj.g@practo.com>
2020-09-02 21:29:52 +0530

    create basic crud for post

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
 import { Post } from "../entities/post";
 import { MyContext } from "../types";
 
@@ -7,5 +7,45 @@ export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(@Arg("id", () => Int) id: number
+    , @Ctx() { em }: MyContext): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title })
+    await em.persistAndFlush(post)
+    return post
+  }
+
+  @Mutation(() => Post)
+  async updatePost(
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id })
+    if (post && typeof title !== undefined) {
+      post.title = title
+      await em.persistAndFlush(post)
+      return post
+    }
+    return null
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Boolean> {
+    await em.nativeDelete(Post, { id })
+    return true
   }
 }
